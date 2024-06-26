7a40ff39387a010b0d1622962bccc2f4538b35fd
Kamva-pro <kamvelihlefatman@gmail.com>
2024-06-26 21:20:46 +0200

    added the knight

---
+++ src/main/java/com/genuineappsgroup/Knight.java
@@ -0,0 +1,34 @@
+package com.genuineappsgroup;
+
+public class Knight extends Piece{
+
+    public Knight(int x, int y, boolean isWhite) {
+        super(x, y, isWhite);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean is_blocked() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_blocked'");
+    }
+
+    @Override
+    public boolean is_check() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_check'");
+    }
+
+    @Override
+    public boolean is_opening() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_opening'");
+    }
+
+    @Override
+    public boolean isValidMove(int newX, int newY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isValidMove'");
+    }
+    
+}
