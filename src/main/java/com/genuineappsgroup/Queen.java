0b8ec0f04b187de7fe5099d7365668eae374f245
Kamva-pro <kamvelihlefatman@gmail.com>
2024-06-26 21:18:19 +0200

    added the queen

---
+++ src/main/java/com/genuineappsgroup/Queen.java
@@ -0,0 +1,35 @@
+package com.genuineappsgroup;
+
+public class Queen extends Piece {
+    int current_x, current_y;
+    int movement_squares;
+    public Queen(int x, int y, boolean isWhite) {
+        super(x, y, isWhite);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean is_blocked() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_blocked'");
+    }
+
+    @Override
+    public boolean is_check() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_check'");
+    }
+
+    @Override
+    public boolean is_opening() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'is_opening'");
+    }
+
+    @Override
+    public boolean isValidMove(int newX, int newY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isValidMove'");
+    }
+    
+}
