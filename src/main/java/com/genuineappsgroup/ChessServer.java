17e22a2ce002fa74cc50ec5f38a8508184a3ac20
Kamva-pro <kamvelihlefatman@gmail.com>
2024-08-29 20:28:23 +0200

    added constructor and run function

--- src/main/java/com/genuineappsgroup/ChessServer.java
+++ src/main/java/com/genuineappsgroup/ChessServer.java
@@ -1,5 +1,38 @@
 package com.genuineappsgroup;
 
-public class ChessServer {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ChessServer implements Runnable {
+    private String clientMachine;
+    private BufferedReader in;
+    private PrintStream out;
     
+
+    public void SimpleServer(Socket socket) throws IOException {
+        clientMachine = socket.getInetAddress().getHostName();
+        out = new PrintStream(socket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        try{
+
+         while(true){
+            String messageFromClient;
+            while((messageFromClient = in.readLine()) != null)
+            {
+
+            }
+        }
+    }catch(IOException exception)
+    {
+        
+    }
+    }
+
 }
